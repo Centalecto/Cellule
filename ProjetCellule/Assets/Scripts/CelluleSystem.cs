ba53c373120c3cda1a67560f6b5b7b9682bb8ae0
Centalecto <kay.boursaud@gmail.com>
2026-01-11 15:19:36 +0100

    test3

---
+++ ProjetCellule/Assets/Scripts/CelluleSystem.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CelluleSystem : MonoBehaviour
+{
+    public GameObject Prefabcellule; 
+    public GameObject PrefabcelluleMuttation;
+
+    private void Update()
+    {
+        
+
+
+
+    }
+}
